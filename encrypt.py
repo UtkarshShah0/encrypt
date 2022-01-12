62643a8f5e6fe3c260efafeab68a8aeea5727eef
code4Y <thecodeyash.gmail.com>
2022-01-12 22:04:39 +0530

    Added Encrypt

---
+++ encrypt.py
@@ -0,0 +1,9 @@
+a=input("Give Me Your Super Secret Password ;) ")
+sum=""
+for i in a:
+    sum=sum+(chr(ord(i)*3))
+print("_^-^_"*10)
+print()
+print("The Encrpyted Super Secret Password is: ",sum)
+print()
+print("_:)_"*12)
